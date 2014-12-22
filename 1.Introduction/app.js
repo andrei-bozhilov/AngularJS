8b59418961f942e2f8e39665e6080aa3aea97145
Andrey Bozhilov <andrei.bozhilov@gmail.com>
2014-12-22 13:20:41 +0200

    add homework

---
+++ 1.Introduction/app.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    angular.module('app', [])
+    .controller('Student', function () {
+        var data =
+        {
+            "name": 'Pesho',
+            "photo": "http://www.nakov.com/wp-content/uploads/2014/05/SoftUni-Logo.png",
+            "grade": 5,
+            "school": "High School of Mathematics",
+            "teacher": "Gichka Pesheva",
+        };
+
+
+        this.data = data;
+    });
+}())
