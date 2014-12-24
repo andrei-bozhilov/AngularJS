1c1f200dd5a2dacf91f498e4dc00caba663b1792
Andrey Bozhilov <andrei.bozhilov@gmail.com>
2014-12-24 17:29:13 +0200

    adding homework

--- 1.Introduction/app.js
+++ 1.Introduction/app.js
@@ -1,16 +1,31 @@
 ﻿(function () {
     angular.module('app', [])
     .controller('Student', function () {
-        var data =
-        {
+        var data = {
             "name": 'Pesho',
             "photo": "http://www.nakov.com/wp-content/uploads/2014/05/SoftUni-Logo.png",
             "grade": 5,
             "school": "High School of Mathematics",
             "teacher": "Gichka Pesheva",
         };
-
-
         this.data = data;
-    });
+
+    })
+
+    .controller('Tiger', function () {
+        var tiger = {
+            name: 'pesho',
+            born: 'africa',
+            birthData: 2006,
+            live: 'sofia zoo',
+            pic: 'http://tigerday.org/wp-content/uploads/2013/04/tiger.jpg'
+        };
+        this.data = tiger;
+    })
+
+    .filter('capitalize', function () {
+        return function (input, all) {
+            return (!!input) ? input.replace(/([^\W_]+[^\s-]*) */g, function (txt) { return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase(); }) : '';
+        }
+    })
 }())
